17cd55086faf9110a8f14caa9c47df37b6caf9e2
XARZZtzy <96812529+XARZZtzy@users.noreply.github.com>
2021-12-30 20:05:59 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -4,24 +4,24 @@ return `DONASI BISA VIA BANK / E-MONEY
 *🏧 BANK*
 
 1. BRI 
-	ㅁ 8881 0887 4350 47326
+	ㅁ -
 2. BCA 
-	ㅁ 3901 0887 4350 47326
+	ㅁ -
 3. BNI 
-	ㅁ 881 0887 4350 47326
+	ㅁ -
 4. PERMATA
-	ㅁ 852 8887 4350 47326
+	ㅁ -
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 0838 3081 5715
+2. PULSA
+	ㅁ 0838 3081 5715
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0838 3081 5715
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama J**i
 `
 	}
 
